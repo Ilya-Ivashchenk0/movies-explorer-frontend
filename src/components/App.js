904ad7366675774e1db6fa328472ce5613d4f4bd
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-10-16 23:54:32 +0300

    components

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom' // , useNavigate, useLocation
+// import Footer from './Footer/Footer'
+// import Header from './Header/Header'
+// import Main from './Main/Main'
+import Login from './Login/Login'
+import Register from './Register/Register'
+// import NotFound from './NotFound/NotFound'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Page</h1>
+      <Routes>
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/saved-movies" element={<Register />} />
+        <Route path="/profile" element={<Register />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
