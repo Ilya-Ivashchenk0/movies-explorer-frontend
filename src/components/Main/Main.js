8d55800f5dcee9e594db4f5488db33d8f2783a93
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-11-02 02:33:08 +0300

    repear main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import './Main.css'
+import Header from '../Header/Header'
 import Promo from './Promo/Promo'
 import Footer from '../Footer/Footer'
 import Techs from './Techs/Techs'
@@ -9,6 +10,7 @@ import Portfolio from './Portfolio/Portfolio'
 function Main() {
   return (
     <div className='main'>
+      <Header />
       <Promo />
       <AboutProject />
       <Techs />
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,11 +1,9 @@
 import './Promo.css'
 import logo from '../../../images/landing-logo.svg'
-import Header from '../../Header/Header'
 
 function Promo() {
   return (
     <div className='promo'>
-      <Header />
       <h2 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h2>
       <img src={logo} alt="Логотип промо блока" className="promo__logo" />
     </div>
