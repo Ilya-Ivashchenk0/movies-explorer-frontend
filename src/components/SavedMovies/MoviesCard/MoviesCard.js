c1b1d08e023477056b59998d78147ed401a9418f
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-11-03 05:22:15 +0300

    next

--- src/components/SavedMovies/MoviesCard/MoviesCard.js
+++ src/components/SavedMovies/MoviesCard/MoviesCard.js
@@ -1,6 +1,5 @@
 import './MoviesCard.css'
-import voidHeart from '../../../images/void-heart.svg'
-// import fullHeart from '../../../images/full-heart.svg'
+import deleteIcon from '../../../images/delete-icon.svg'
 
 function MoviesCard({ img, name, time }) {
   return (
@@ -8,7 +7,7 @@ function MoviesCard({ img, name, time }) {
       <img className='movies-card__img' src={img} alt="Фото фильма" />
       <div className='movies-card__board'>
         <h4 className='movies-card__name'>{ name }</h4>
-        <button className='movies-card__button'><img className='movies-card__icon' src={voidHeart} alt="like icon" /></button>
+        <button className='movies-card__button'><img className='movies-card__icon' src={deleteIcon} alt="like icon" /></button>
       </div>
       <p className='movies-card__time'>{ time }</p>
     </article>
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,8 +1,19 @@
 import './SavedMovies.css'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import SearchForm from '../Movies/SearchForm/SearchForm'
+import MoviesCardList from './MoviesCardList/MoviesCardList'
+import VoidBlock from './VoidBlock/VoidBlock'
 
 function SavedMovies() {
   return (
-    <div />
+    <div>
+      <Header />
+      <SearchForm />
+      <MoviesCardList />
+      <VoidBlock />
+      <Footer />
+    </div>
   )
 }
 
