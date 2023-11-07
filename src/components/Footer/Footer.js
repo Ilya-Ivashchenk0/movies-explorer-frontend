8154e9ef48b1be2d63fc9f21478b8a7ccd8261db
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-11-07 18:14:05 +0300

    next

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import './Footer.css'
 
 function Footer() {
+  const year = new Date().getFullYear()
+
   return (
     <footer className='footer'>
       <p className='footer__heading'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className='footer__info'>
-        <p className='footer__copyright'>© 2020</p>
+        <p className='footer__copyright'>© {year}</p>
         <ul className='footer__links'>
-          <li className='footer__link-element'><a href='https://practicum.yandex.ru' className='footer__link hover-element-link'>Яндекс.Практикум</a></li>
-          <li className='footer__link-element'><a href='https://github.com' className='footer__link hover-element-link'>Github</a></li>
+          <li className='footer__link-element'><a href='https://practicum.yandex.ru' target='blank' className='footer__link hover-element-link'>Яндекс.Практикум</a></li>
+          <li className='footer__link-element'><a href='https://github.com' target='blank' className='footer__link hover-element-link'>Github</a></li>
         </ul>
       </div>
     </footer>
--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -10,7 +10,7 @@ function AboutMe() {
           <h3 className='about-me__name'>Виталий</h3>
           <h4 className='about-me__profession'>Фронтенд-разработчик, 30 лет</h4>
           <p className='about-me__about'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <a className='about-me__git' href='https://github.com'>Github</a>
+          <a className='about-me__git' href='https://github.com' target='blank'>Github</a>
         </div>
         <img className='about-me__avatar' src={avatar} alt='Аватар автора' />
       </div>
