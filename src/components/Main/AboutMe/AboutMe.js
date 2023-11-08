1a8e3067a8b0a99183d64b7ed0b0008165f6a7b0
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-11-08 15:28:05 +0300

    next

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -7,9 +7,9 @@ function AboutMe() {
       <h2 className='about-me__title'>Студент</h2>
       <div className='about-me__block'>
         <div className='about-me__info'>
-          <h3 className='about-me__name'>Виталий</h3>
-          <h4 className='about-me__profession'>Фронтенд-разработчик, 30 лет</h4>
-          <p className='about-me__about'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <h3 className='about-me__name'>Илья</h3>
+          <h4 className='about-me__profession'>Фронтенд-разработчик, 27 лет</h4>
+          <p className='about-me__about'>Я родился в Краснодаре, но 1,5 года назад переехал в Санкт-Петербург. Закончил факультет Природообустройства и Водопользования в КубГАУ. Начал свой путь в вебе примерно 2 года назад. За время учебы в Практикуме успел поработать в проекте агрегатора по доставке еды. </p>
           <a className='about-me__git hover-element' href='https://github.com' target='blank'>Github</a>
         </div>
         <img className='about-me__avatar' src={avatar} alt='Аватар автора' />
