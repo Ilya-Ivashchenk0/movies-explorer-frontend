5bb931202ea5c2175bdfbe79a6e7cd7f16b449a1
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-02-01 16:55:03 +0300

    next

--- src/utils/checkProd.js
+++ src/utils/checkProd.js
@@ -1,7 +1,7 @@
 export const baseUrl = () => {
   if (process.env.NODE_ENV !== 'production') {
-    return `http://localhost:3000`
+    return `http://localhost:3500`
   } else {
-    return `https://api.movie.explorer.nomoredomainsrocks.ru`
+    return `https://movies-explorer-api.test-tasks.ru`
   }
 }
